27ca115e29ecb7ec463a61ef014f4ffda90508ce
lab <lab@lab117>
2017-10-25 11:13:37 +0200

    change bash script for server
    use non-local names for script

--- server-arduino.sh
+++ server-arduino.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-if [ "$(hostname)" == "raspberrypi" ]
-then
-    python /home/pi/programs/swnt-reactor/src/serverArduino.py
-fi
-if [ "$(hostname)" == "lab117" ]
-then
-    python /home/lab/programs/swnt-reactor/src/serverArduino.py
-fi
-if [ "$(hostname)" == "uk-work" ]
-then
-    python /home/matthias/Documents/programs/python/swnt-reactor/src/serverArduino.py
-fi
-
+DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+python $DIR/src/serverArduino.py
